e2ce047926b35d6154ece9ec0d7aeac48bdf58b5
orbxball <orbxball@protonmail.com>
2017-04-14 10:57:27 +0800

    cnn+dropout: reproduce: 0.60379

--- hw3/train_cnn.py
+++ hw3/train_cnn.py
@@ -12,10 +12,9 @@ from keras import optimizers
 height = width = 48
 num_classes = 7
 input_shape = (height, width, 1)
-batch_size = 32
+batch_size = 128
 epochs = 20
-pool_size = (2, 2)
-model_name = 'cnn_d3.h5'
+model_name = 'try.h5'
 isValid = 1
 
 # Read the train data
@@ -33,26 +32,20 @@ with open(sys.argv[1], "r+") as f:
 # Change data into CNN format
 X = X.reshape(X.shape[0], height, width, 1)
 
-# Split data
-if isValid:
-  valid_num = 1000
-  X_train, Y_train = X[:-valid_num], Y[:-valid_num]
-  X_valid, Y_valid = X[-valid_num:], Y[-valid_num:]
-else:
-  X_train, Y_train = X, Y
+X_train, Y_train = X, Y
 
 # Construct the model
 model = Sequential()
-model.add(Conv2D(25, (3, 3), activation='relu', input_shape=input_shape))
-model.add(Conv2D(50, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=pool_size))
-model.add(Dropout(0.25))
-model.add(Conv2D(50, (3, 3), activation='relu'))
-model.add(Conv2D(100, (3, 3), activation='relu'))
-model.add(AveragePooling2D(pool_size=pool_size))
-model.add(Dropout(0.25))
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(3, 3)))
+model.add(Dropout(0.5))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(256, (3, 3), activation='relu'))
+model.add(AveragePooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
 model.add(Flatten())
-model.add(Dense(500, activation='relu'))
+model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes))
 model.add(Activation('softmax'))
@@ -69,20 +62,12 @@ if isValid:
             batch_size=batch_size,
             epochs=epochs,
             verbose=1,
-            validation_data=(X_valid, Y_valid))
+            validation_split=0.2)
 else:
   model.fit(X_train, Y_train,
             batch_size=batch_size,
             epochs=epochs,
             verbose=1)
-
-# Evaluate the test data
-if isValid:
-  score = model.evaluate(X_valid, Y_valid, verbose=0)
-else:
-  score = model.evaluate(X_train, Y_train, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
 
 # Save model
 model.save(model_name)
