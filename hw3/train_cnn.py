a6818f9880e174dfa38b59ee028937e1a144dedf
orbxball <orbxball@protonmail.com>
2017-04-16 08:25:04 +0800

    batch normalization + padding: 1.0246/0.6757 -> 0.68515

--- hw3/train_cnn.py
+++ hw3/train_cnn.py
@@ -6,6 +6,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Dropout, LeakyReLU
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
+from keras.layers import BatchNormalization
 from keras import losses
 from keras import optimizers
 
@@ -14,9 +15,9 @@ height = width = 48
 num_classes = 7
 input_shape = (height, width, 1)
 batch_size = 128
-epochs = 100
+epochs = 50
 zoom_range = 0.05
-model_name = 'pre2.h5'
+model_name = 'pre3.h5'
 isValid = 1
 
 # Read the train data
@@ -45,29 +46,33 @@ else:
 
 # Construct the model
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), padding='same', input_shape=input_shape))
 model.add(LeakyReLU(alpha=0.03))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+model.add(Dropout(0.3))
 
-model.add(Conv2D(64, (3, 3)))
+model.add(Conv2D(64, (3, 3), padding='same'))
 model.add(LeakyReLU(alpha=0.03))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+model.add(Dropout(0.3))
 
-model.add(Conv2D(128, (3, 3)))
+model.add(Conv2D(128, (3, 3), padding='same'))
 model.add(LeakyReLU(alpha=0.03))
-model.add(AveragePooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(BatchNormalization())
+model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
+model.add(Dropout(0.3))
 
-model.add(Conv2D(256, (3, 3)))
+model.add(Conv2D(256, (3, 3), padding='same'))
 model.add(LeakyReLU(alpha=0.03))
-model.add(AveragePooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(BatchNormalization())
+model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
+model.add(Dropout(0.3))
 
 model.add(Flatten())
 
-model.add(Dense(128, activation='relu'))
+model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes))
 model.add(Activation('softmax'))
